d60318415b3d56a188214200928e2739812acd3b
Dương Thanh Phụng <20130099@st.hcmuaf.edu.vn>
2024-01-10 20:43:29 +0700

    Thêm API lấy lấy danh sách sản phẩm (user), tìm kiếm sp

---
+++ Controllers/Helper/ShowSanphamController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebSellPhoneAPI.Entities;
+
+namespace WebSellPhoneAPI.Controllers.Helper
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShowSanphamController : ControllerBase
+    {
+        private readonly SellPhoneContext _context;
+
+        public ShowSanphamController(SellPhoneContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<object>>> GetSanphams()
+        {
+            if (_context.Sanphams == null)
+            {
+                return NotFound();
+            }
+
+            var groupedProducts = _context.Sanphams.GroupBy(p => p.Tenviettat)
+                                                    .Select(group => new
+                                                    {
+                                                        Tenviettat = group.Key,
+                                                        Thongtin = group.First(),
+                                                        PhanLoai = group.Select(p => new
+                                                        {
+                                                            Id = p.Id,
+                                                            TenSp = p.TenSp,
+                                                            Giagoc = p.Giagoc,
+                                                            Giadagiam = p.Giadagiam,
+                                                            Mausanpham = p.Mausanpham,
+                                                            Ram = p.Ram,
+                                                            Dungluong = p.Dungluong,
+                                                        })
+                                                    });
+
+            return await Task.FromResult(groupedProducts.ToList());
+        }
+
+        [HttpGet("Search")]
+        public async Task<ActionResult<IEnumerable<object>>> GetSanphams([FromQuery] string tenviettat = null,
+                                                                         [FromQuery] string order = null,
+                                                                         [FromQuery] int page = 1,
+                                                                         [FromQuery] int range = 10)
+        {
+            if (_context.Sanphams == null)
+            {
+                return NotFound();
+            }
+
+            var groupedProducts = _context.Sanphams.GroupBy(p => p.Tenviettat)
+                .Select(group => new
+                {
+                    Tenviettat = group.Key,
+                    Thongtin = group.First(),
+                    Gia = group.First().Giadagiam,
+                    PhanLoai = group.Select(p => new
+                    {
+                        Id = p.Id,
+                        TenSp = p.TenSp,
+                        Giagoc = p.Giagoc,
+                        Giadagiam = p.Giadagiam,
+                        Mausanpham = p.Mausanpham,
+                        Ram = p.Ram,
+                        Dungluong = p.Dungluong,
+                    })
+                });
+
+            var filteredProducts = groupedProducts.Where(group => string.IsNullOrEmpty(tenviettat) || group.Tenviettat.Contains(tenviettat));
+
+            if (!string.IsNullOrEmpty(order))
+            {
+                bool isDescending = order.ToLower() == "desc";
+                filteredProducts = isDescending
+                    ? filteredProducts.OrderByDescending(group => group.Gia)
+                    : filteredProducts.OrderBy(p => p.Gia);
+            }
+
+            var pagedProducts = filteredProducts.Skip((page - 1) * range)
+                                                .Take(range);
+
+            return await Task.FromResult(pagedProducts.ToList());
+        }
+
+        // GET: api/ShowSanpham/iphone-11
+        [HttpGet("{tenviettat}")]
+        public async Task<ActionResult<object>> GetSanpham(string tenviettat)
+        {
+            if (_context.Sanphams == null)
+            {
+                return NotFound();
+            }
+            var sanphams = await _context.Sanphams.Where(sp => sp.Tenviettat == tenviettat).ToListAsync();
+
+            if (sanphams.Count == 0)
+            {
+                return NotFound();
+            }
+
+            var sanpham = new
+            {
+                Tenviettat = sanphams.First().Tenviettat,
+                Thongtin = sanphams.First(),
+                PhanLoai = sanphams.Select(p => new
+                {
+                    Id = p.Id,
+                    TenSp = p.TenSp,
+                    Giagoc = p.Giagoc,
+                    Giadagiam = p.Giadagiam,
+                    Mausanpham = p.Mausanpham,
+                    Ram = p.Ram,
+                    Dungluong = p.Dungluong,
+                })
+            };
+
+
+            return sanpham;
+        }
+
+        private bool SanphamExists(int id)
+        {
+            return (_context.Sanphams?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
