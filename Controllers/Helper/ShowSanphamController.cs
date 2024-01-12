a4b82c48bae5509878efc8dffb9fa29ed3c63408
Dương Thanh Phụng <20130099@st.hcmuaf.edu.vn>
2024-01-12 22:35:23 +0700

    abc

--- Controllers/Helper/ShowSanphamController.cs
+++ Controllers/Helper/ShowSanphamController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using WebSellPhoneAPI.Entities;
+using WebSellPhoneAPI.Models;
 
 namespace WebSellPhoneAPI.Controllers.Helper
 {
